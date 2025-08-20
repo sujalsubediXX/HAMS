280736e36b500027c9739fda92d46796d36506b7
SujalSubedi <sujalsubedi541@gmail.com>
2025-08-20 11:01:30 +0545

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'https://hams-eegi.onrender.com', // ✅ your Render backend
+        target: 'https://hams-eegi.onrender.com', 
         changeOrigin: true,
         secure: false,
         configure: (proxy, options) => {
