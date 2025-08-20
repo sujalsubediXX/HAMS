bd69831f6e1982359b2457dda2c4c9f39faca5f1
SujalSubedi <sujalsubedi541@gmail.com>
2025-08-20 19:54:12 +0545

    Update Patients.jsx

--- frontend/src/pages/doctor/Patients.jsx
+++ frontend/src/pages/doctor/Patients.jsx
@@ -16,7 +16,7 @@ const Patients = () => {
       if (user?.email) {
         try {
           const res = await axios.get(
-            "/api/appointment/patients-by-doctor-email",
+            "https://hams-eegi.onrender.com/api/appointment/patients-by-doctor-email",
             {
               params: { email: user.email },
             }
